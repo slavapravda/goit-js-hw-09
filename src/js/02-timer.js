bff63442bcf163a8a1d29ec6f820cded8a0d045d
Slava Pravda <pravdaslava7@gmail.com>
2022-07-18 13:06:28 +0300

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,28 +12,9 @@ const hoursEl = document.querySelector('[data-hours]');
 const minutesEl = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
 
-
 btnStartEl.disabled = true;
 
 let selectedTime = null;
-
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = pad(Math.floor(ms / day));
-  const hours = pad(Math.floor((ms % day) / hour));
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-
-  return { days, hours, minutes, seconds };
-}
-
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
 
 const options = {
   enableTime: true,
@@ -64,7 +45,7 @@ class Timer {
       return;
     }
     this.isAcive = true;
-    this.timerId = setInterval(() => {
+    const timerId = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = selectedTime - currentTime;
 
@@ -81,7 +62,25 @@ class Timer {
   }
 
   stop() {
-    clearInterval(this.timerId);
+    clearInterval(timerId);
+  }
+
+  convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = pad(Math.floor(ms / day));
+    const hours = pad(Math.floor((ms % day) / hour));
+    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+    return { days, hours, minutes, seconds };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
   }
 }
 
